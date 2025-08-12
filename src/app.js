784d86ff74674d49f17fb3133d1e009324ea7532
Thiago Pereira Sousa <JT0020362@MATEUS.DC>
2025-08-12 15:12:14 -0300

    create : added some images to the projects part and need to fix the responsitive

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
 
 
 
+
 document.getElementById('btnDownload').addEventListener("click", function (){
 
-
     const urlCurriculo = "curriculo.pdf";
-
+    
     const link = document.createElement("a");
 
     link.href = urlCurriculo;
-
 
     link.download = "Thiago-sousa-curriculo.pdf";
 
@@ -18,4 +17,13 @@ document.getElementById('btnDownload').addEventListener("click", function (){
     link.click();
 
     document.body.removeChild(link);
+})
+
+//pegar a menu e a side bar,para quando o usuario clicar no menu aparecer a side bar
+
+const menu = document.getElementById("menu");
+const sideBar = document.getElementById("side-bar");
+
+menu.addEventListener("click" , function () {
+  sideBar.classList.show('side-bar');
 })
